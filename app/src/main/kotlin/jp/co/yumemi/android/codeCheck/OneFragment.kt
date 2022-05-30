62c48169ce9646051b0d43ca0ed112b0020f43ed
nissy-yuki <nisi20010510@gmail.com>
2022-05-30 17:51:02 +0900

    TwoFragmentのbindingをライフサイクルの中へ

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/OneFragment.kt
@@ -20,11 +20,11 @@ class OneFragment : Fragment(R.layout.fragment_one) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentOneBinding.bind(view)
-        val viewModel = OneViewModel(context!!)
+        val viewModel = OneViewModel(requireContext())
 
-        val layoutManager = LinearLayoutManager(context!!)
+        val layoutManager = LinearLayoutManager(requireContext())
         val dividerItemDecoration =
-            DividerItemDecoration(context!!, layoutManager.orientation)
+            DividerItemDecoration(requireContext(), layoutManager.orientation)
         val adapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
             override fun itemClick(item: GitItem) {
                 gotoRepositoryFragment(item)

050eb2e7bc595ea656409e0c4d6e289f1974f194
nissy-yuki <nisi20010510@gmail.com>
2022-06-03 16:17:59 +0900

    過去のコードを削除

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/OneViewModel.kt
@@ -47,53 +47,13 @@ class OneViewModel @Inject constructor() : ViewModel() {
     // 検索結果
     fun searchResults(inputText: String) = runBlocking {
 
-//        val client = HttpClient(Android)
-//        return@runBlocking viewModelScope.async {
-//            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-//                header("Accept", "application/vnd.github.v3+json")
-//                parameter("q", inputText)
-//            } ?: return@async emptyList()
-//
-//            val jsonBody = JSONObject(response.receive<String>())
-//            val jsonItems = jsonBody.optJSONArray("items") ?: return@async emptyList()
-//            val items = mutableListOf<GitItem>()
-//
-//            // jsonItemsの数分ループ
-//            for (i in 0 until jsonItems.length()) {
-//                val jsonItem = jsonItems.optJSONObject(i) ?: continue
-//                val name = jsonItem.optString("full_name")
-//                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-//                val language = jsonItem.optString("language")
-//                val stargazersCount = jsonItem.optLong("stargazers_count")
-//                val watchersCount = jsonItem.optLong("watchers_count")
-//                val forksCount = jsonItem.optLong("forks_conut")
-//                val openIssuesCount = jsonItem.optLong("open_issues_count")
-//
-//                items.add(
-//                    GitItem(
-//                        name = name,
-//                        ownerIconUrl = ownerIconUrl,
-//                        language = (languageFormat.format(language)),
-//                        stargazersCount = stargazersCount,
-//                        watchersCount = watchersCount,
-//                        forksCount = forksCount,
-//                        openIssuesCount = openIssuesCount
-//                    )
-//                )
-//            }
-//
-//
-//            return@async items.toList()
-//        }.await()
         viewModelScope.launch {
             try {
-                Log.d("hoge","hoge")
-                Log.d("checkValue",repository.searchRepository(inputText).toString())
                 val response = repository.searchRepository(inputText)
                 if(response.isSuccessful){
                     _searchResult.value = response.body()
                 } else {
-
+                    Log.d("get api","not success")
                 }
             } catch (e: Exception){
                 Log.d("Get api",e.toString())
